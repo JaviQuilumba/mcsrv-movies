7fb11d5b01e3b88876b7af84e19486cd7e8fe881
JaviQuilumba <javier0611quilumba@gmail.com>
2024-07-21 17:34:02 -0500

    6 commit

--- view-movies/server.js
+++ view-movies/server.js
@@ -6,7 +6,7 @@ const movieRoutes = require('./routes/movieRoutes');
 const cors = require('cors');
 
 const app = express();
-const port = 4004;
+const port = 4000;
 
 app.get('/', (req, res) => {
   res.status(200).json({ status: 'ok', message: 'Microservice view-movies Running :)' });
